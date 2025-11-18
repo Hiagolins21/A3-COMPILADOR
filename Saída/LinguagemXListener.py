e5472ffe48a5cebe7f159c1e055cc1f5dfd504ad
Hiago Lins <138394006+Hiagolins21@users.noreply.github.com>
2025-11-17 22:52:40 -0300

    Add files via upload

---
+++ Saída/LinguagemXListener.py
@@ -0,0 +1,165 @@
+# Generated from LinguagemX.g4 by ANTLR 4.13.2
+from antlr4 import *
+if "." in __name__:
+    from .LinguagemXParser import LinguagemXParser
+else:
+    from LinguagemXParser import LinguagemXParser
+
+# This class defines a complete listener for a parse tree produced by LinguagemXParser.
+class LinguagemXListener(ParseTreeListener):
+
+    # Enter a parse tree produced by LinguagemXParser#OpMultDiv.
+    def enterOpMultDiv(self, ctx:LinguagemXParser.OpMultDivContext):
+        pass
+
+    # Exit a parse tree produced by LinguagemXParser#OpMultDiv.
+    def exitOpMultDiv(self, ctx:LinguagemXParser.OpMultDivContext):
+        pass
+
+
+    # Enter a parse tree produced by LinguagemXParser#OpSomaSub.
+    def enterOpSomaSub(self, ctx:LinguagemXParser.OpSomaSubContext):
+        pass
+
+    # Exit a parse tree produced by LinguagemXParser#OpSomaSub.
+    def exitOpSomaSub(self, ctx:LinguagemXParser.OpSomaSubContext):
+        pass
+
+
+    # Enter a parse tree produced by LinguagemXParser#ExpressaoTermo.
+    def enterExpressaoTermo(self, ctx:LinguagemXParser.ExpressaoTermoContext):
+        pass
+
+    # Exit a parse tree produced by LinguagemXParser#ExpressaoTermo.
+    def exitExpressaoTermo(self, ctx:LinguagemXParser.ExpressaoTermoContext):
+        pass
+
+
+    # Enter a parse tree produced by LinguagemXParser#OpRelacional.
+    def enterOpRelacional(self, ctx:LinguagemXParser.OpRelacionalContext):
+        pass
+
+    # Exit a parse tree produced by LinguagemXParser#OpRelacional.
+    def exitOpRelacional(self, ctx:LinguagemXParser.OpRelacionalContext):
+        pass
+
+
+    # Enter a parse tree produced by LinguagemXParser#TermoID.
+    def enterTermoID(self, ctx:LinguagemXParser.TermoIDContext):
+        pass
+
+    # Exit a parse tree produced by LinguagemXParser#TermoID.
+    def exitTermoID(self, ctx:LinguagemXParser.TermoIDContext):
+        pass
+
+
+    # Enter a parse tree produced by LinguagemXParser#TermoNumInt.
+    def enterTermoNumInt(self, ctx:LinguagemXParser.TermoNumIntContext):
+        pass
+
+    # Exit a parse tree produced by LinguagemXParser#TermoNumInt.
+    def exitTermoNumInt(self, ctx:LinguagemXParser.TermoNumIntContext):
+        pass
+
+
+    # Enter a parse tree produced by LinguagemXParser#TermoNumDec.
+    def enterTermoNumDec(self, ctx:LinguagemXParser.TermoNumDecContext):
+        pass
+
+    # Exit a parse tree produced by LinguagemXParser#TermoNumDec.
+    def exitTermoNumDec(self, ctx:LinguagemXParser.TermoNumDecContext):
+        pass
+
+
+    # Enter a parse tree produced by LinguagemXParser#TermoTexto.
+    def enterTermoTexto(self, ctx:LinguagemXParser.TermoTextoContext):
+        pass
+
+    # Exit a parse tree produced by LinguagemXParser#TermoTexto.
+    def exitTermoTexto(self, ctx:LinguagemXParser.TermoTextoContext):
+        pass
+
+
+    # Enter a parse tree produced by LinguagemXParser#TermoParenteses.
+    def enterTermoParenteses(self, ctx:LinguagemXParser.TermoParentesesContext):
+        pass
+
+    # Exit a parse tree produced by LinguagemXParser#TermoParenteses.
+    def exitTermoParenteses(self, ctx:LinguagemXParser.TermoParentesesContext):
+        pass
+
+
+    # Enter a parse tree produced by LinguagemXParser#programa.
+    def enterPrograma(self, ctx:LinguagemXParser.ProgramaContext):
+        pass
+
+    # Exit a parse tree produced by LinguagemXParser#programa.
+    def exitPrograma(self, ctx:LinguagemXParser.ProgramaContext):
+        pass
+
+
+    # Enter a parse tree produced by LinguagemXParser#declaracao.
+    def enterDeclaracao(self, ctx:LinguagemXParser.DeclaracaoContext):
+        pass
+
+    # Exit a parse tree produced by LinguagemXParser#declaracao.
+    def exitDeclaracao(self, ctx:LinguagemXParser.DeclaracaoContext):
+        pass
+
+
+    # Enter a parse tree produced by LinguagemXParser#comando.
+    def enterComando(self, ctx:LinguagemXParser.ComandoContext):
+        pass
+
+    # Exit a parse tree produced by LinguagemXParser#comando.
+    def exitComando(self, ctx:LinguagemXParser.ComandoContext):
+        pass
+
+
+    # Enter a parse tree produced by LinguagemXParser#atribuicao.
+    def enterAtribuicao(self, ctx:LinguagemXParser.AtribuicaoContext):
+        pass
+
+    # Exit a parse tree produced by LinguagemXParser#atribuicao.
+    def exitAtribuicao(self, ctx:LinguagemXParser.AtribuicaoContext):
+        pass
+
+
+    # Enter a parse tree produced by LinguagemXParser#io.
+    def enterIo(self, ctx:LinguagemXParser.IoContext):
+        pass
+
+    # Exit a parse tree produced by LinguagemXParser#io.
+    def exitIo(self, ctx:LinguagemXParser.IoContext):
+        pass
+
+
+    # Enter a parse tree produced by LinguagemXParser#se_senao.
+    def enterSe_senao(self, ctx:LinguagemXParser.Se_senaoContext):
+        pass
+
+    # Exit a parse tree produced by LinguagemXParser#se_senao.
+    def exitSe_senao(self, ctx:LinguagemXParser.Se_senaoContext):
+        pass
+
+
+    # Enter a parse tree produced by LinguagemXParser#loop_while.
+    def enterLoop_while(self, ctx:LinguagemXParser.Loop_whileContext):
+        pass
+
+    # Exit a parse tree produced by LinguagemXParser#loop_while.
+    def exitLoop_while(self, ctx:LinguagemXParser.Loop_whileContext):
+        pass
+
+
+    # Enter a parse tree produced by LinguagemXParser#loop_for.
+    def enterLoop_for(self, ctx:LinguagemXParser.Loop_forContext):
+        pass
+
+    # Exit a parse tree produced by LinguagemXParser#loop_for.
+    def exitLoop_for(self, ctx:LinguagemXParser.Loop_forContext):
+        pass
+
+
+
+del LinguagemXParser
